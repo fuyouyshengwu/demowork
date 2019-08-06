fce336f8606c6477c4849610d3bbb049c202144c
sanshengwanwu <48247081+fuyouyshengwu@users.noreply.github.com>
2019-08-06 22:29:21 +0800

    Update main.go

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 
 func main(){
 	http.HandleFunc("/index", func(writer http.ResponseWriter, request *http.Request) {
-		s := "<h1 >it is v3</h2>"
+		s := "<h1 >it is v2</h2>"
 		writer.Write([]byte(s))
 	})
 	http.HandleFunc("/env", func(writer http.ResponseWriter, request *http.Request) {
