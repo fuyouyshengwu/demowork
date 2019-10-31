ced40b5a2dfe306d3115f62f5bc8b72669931b34
张国猛 <zhangguom@inspur.com>
2019-10-31 20:27:20 +0800

    cloudevent

--- main.go
+++ main.go
@@ -1,26 +1,43 @@
 package main
 
 import (
-	"net/http"
-	"os"
+	"eventing/pkg/kncloudevents"
+	"github.com/prometheus/common/log"
+	"context"
 	"fmt"
+	"github.com/cloudevents/sdk-go-master/pkg/cloudevents"
+	"net/http"
+	"io/ioutil"
 )
-
 func main(){
-	http.HandleFunc("/index", func(writer http.ResponseWriter, request *http.Request) {
-		s := "<h1 >it is v1</h2>"
-		writer.Write([]byte(s))
-	})
-	http.HandleFunc("/env", func(writer http.ResponseWriter, request *http.Request) {
-		home := os.Getenv("env")
-		writer.Write([]byte(home))
-	})
-	http.HandleFunc("/healthz", func(writer http.ResponseWriter, request *http.Request) {
-		fmt.Println("I am ok")
-	})
-	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
-		fmt.Println("I am ok")
-	})
-	fmt.Println("working at 8080")
-	http.ListenAndServe("0.0.0.0:8080",nil)
+	c, err := kncloudevents.NewDefaultClient()
+	if err !=nil{
+		log.Fatal("Failed to create client, ", err)
+	}
+	log.Fatal(c.StartReceiver(context.Background(), display))
 }
+func display(event cloudevents.Event) {
+
+	fmt.Printf("☁️  cloudevents.Event\n%s", event.String())
+	requestUrl := ""
+	client := &http.Client{Transport:&http.Transport{}}
+	req, err := http.NewRequest("GET",requestUrl,nil)
+	if err != nil {
+		log.Errorf("req:%v", err)
+	}
+	query:= req.URL.Query()
+	req.URL.RawQuery = query.Encode()
+	response, err := client.Do(req)
+	if err != nil {
+		log.Error("Response error ", err)
+	}
+	defer response.Body.Close()
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Error("Get response body fail ", err)
+
+	}
+	log.Debug("string result : %s", string(body))
+	log.Debug("end!")
+}
+
